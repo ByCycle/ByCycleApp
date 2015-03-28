2836ac5153c457d19d115859349464ad7aa37183
Florian Rathgeber <florian.rathgeber@gmail.com>
2015-03-28 17:03:42 +0000

    Factor directives into dedicated module

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -225,33 +225,4 @@ angular.module('starter.controllers', ['starter.services'])
   },function(error){
     console.log(error);
   });
-})
-.directive('backImg', function(){
-    return function(scope, element, attrs){
-        var url = attrs.backImg;
-        var content = element.find('a');
-        content.css({
-            'background': 'url(' + url +')',
-            'background-size': '100% 100%',
-            'height':'150px',
-            '-webkit-filter':'blur(0.5px)',
-            'filter':'blur(0.5px)'
-        });
-    };
-})
-.directive( 'goToPath', function ( $location ) {
-  return function ( scope, element, attrs ) {
-    var path;
-
-    attrs.$observe( 'goToPath', function (val) {
-      path = val;
-    });
-
-    element.bind( 'click', function () {
-      scope.$apply( function () {
-        console.log(path);
-        $location.path( path );
-      });
-    });
-  };
 });
